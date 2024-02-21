3d5e3a84d4a0e0fff954ba030e3922b55890f966
garelickja <138186599+garelickja@users.noreply.github.com>
2024-02-21 15:05:43 -0600

    dog ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+    const breedList = document.getElementById('dog-breeds');
+    let allBreeds = [];
+
+    function fetchImages() {
+        fetch(imgUrl)
+            .then(res => res.json())
+            .then(data => {
+                const images = data.message;
+                const container = document.getElementById('dog-image-container');
+                images.forEach(imageUrl => {
+                    const img = document.createElement('img');
+                    img.src = imageUrl;
+                    container.appendChild(img);
+                });
+            })
+    };
+
+    function fetchBreeds() {
+        fetch(breedUrl)
+            .then(res => res.json())
+            .then(data => {
+                allBreeds = Object.keys(data.message); 
+                allBreeds.forEach(breed => {
+                    const li = document.createElement('li');
+                    li.textContent = breed;
+                    breedList.appendChild(li);
+                });
+            })
+    };
+
+    function filterBreedsByLetter(letter) {
+        const filteredBreeds = allBreeds.filter(breed => breed.startsWith(letter));
+        breedList.innerHTML = '';
+
+        filteredBreeds.forEach(breed => {
+            const li = document.createElement('li');
+            li.textContent = breed;
+            breedList.appendChild(li);
+        });
+    };
+
+    function handleBreedClick(e) {
+        e.target.style.color = 'red';
+    };
+
+    function handleDropdownChange(e) {
+        const letter = e.target.value;
+        filterBreedsByLetter(letter);
+    };
+
+    fetchImages();
+    fetchBreeds();
+
+    breedList.addEventListener('click', handleBreedClick);
+    document.getElementById('breed-dropdown').addEventListener('change', handleDropdownChange);
+});
